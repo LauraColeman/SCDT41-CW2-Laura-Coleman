2b0e66942d8cf06162c5aab49d9590301e0e332e
LauraColeman <57405611+LauraColeman@users.noreply.github.com>
2020-04-17 15:52:08 +0100

    Add files via upload
    Set up classes. Set up abstract class for dentist and nurse details. Added inherited constructor from abstract class to dentist and nurse.

---
+++ Staff.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW2
+{
+    abstract class Staff
+    {
+
+        //Shared values between nurses and dentists. 
+        //Can be accessed from nurse and dentist classes.
+
+        private string name;
+        private string practice;
+        private int room;
+        private string username;
+        private string password;
+
+
+        public string staffName
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string staffPractice
+        {
+            get { return practice; }
+            set { practice = value; }
+        }
+
+        public int staffRoom
+        {
+            get { return room; }
+            set { room = value; }
+        }
+
+        public string userName
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string userPassword
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        //Constructor for nurses and dentists
+
+        public Staff(string name, string practice, int room, string username, string password)
+        {
+            this.name = name;
+            this.practice = practice;
+            this.room = room;
+            this.username = username;
+            this.password = password;
+
+
+
+        }
+
+    }
+}
---
+++ nurse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW2
+{
+    class Nurse : Staff 
+    {
+        //base to access constructor from Staff abstract class.
+        public Nurse(string name, string practice, int room, string username, string password) :base(name, practice, room, username, password)
+
+        {
+            
+        }
+
+        //constructor from abstract class
+
+        Nurse Nurse1 = new Nurse ("Jane Doe", "Taunton", 1, "jane","jane"); 
+        Nurse Nurse2 = new Nurse("Jane Doe", "Taunton", 2, "jane", "jane");
+        Nurse Nurse3 = new Nurse("Jane Doe", "Taunton", 3, "jane", "jane");
+        Nurse Nurse4 = new Nurse("Jane Doe", "Street", 1, "jane", "jane");
+        Nurse Nurse5 = new Nurse("Jane Doe", "Street", 2, "jane", "jane");
+        Nurse Nurse6 = new Nurse("Jane Doe", "Street", 3, "jane", "jane");
+        Nurse Nurse7 = new Nurse("Jane Doe", "Wells", 1, "jane", "jane");
+        Nurse Nurse8 = new Nurse("Jane Doe", "Wells", 2, "jane", "jane");
+        Nurse Nurse9 = new Nurse("Jane Doe", "Wells", 3, "jane", "jane");
+
+
+    }
+}
