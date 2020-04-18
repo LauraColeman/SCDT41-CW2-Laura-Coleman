0bef6a6cb9665c58c42ea2216fceba8c933a425b
LauraColeman <57405611+LauraColeman@users.noreply.github.com>
2020-04-18 16:37:20 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,55 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace CW2
 {
     class Program
     {
+     
+
+
         static void Main(string[] args)
         {
 
-            //System Admin Log In
+
+            Console.WriteLine("Press 1 for admin access. Press 2 for nurse access. Press 3 for doctor access");
+
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Admin admin = new Admin("administrator", "admin", "admin");
+                admin.adminLogIn();
+
+
+            }
+
+            else if (choice == "2")
+            {
+
+                Test Nurse1 = new Test();
+                Nurse1.LogIn2();
+
+
+
+            }
+
+
+            else if(choice == "3")
+            {
+
+
+            }
+
+
+            //Add other choice for patients?
+            
 
 
 
 
-            //Dentists Log In
-
-            dentist den1 = new dentist("Dennis Doe", "Taunton", 1, "dennis", "dennis");
-            dentist den2 = new dentist("Dennis Two", "Taunton", 2, "dennistwo", "dennistwo");
-            dentist den3 = new dentist("Dennis Three", "Taunton", 3, "dennisthree", "dennisthree");
-            dentist den4 = new dentist("Dennis Four", "Street", 1, "dennisfour", "dennisfour");
-            dentist den5 = new dentist("Dennis Five", "Street", 2, "dennisfive", "dennisfive");
-            dentist den6 = new dentist("Dennis Six", "Street", 3, "dennissix", "dennissix");
-            dentist den7 = new dentist("Dennis Seven", "Wells", 1, "dennisseven", "dennisseven");
-            dentist den8 = new dentist("Dennis Eight", "Wells", 2, "denniseight", "denniseight");
-            dentist den9 = new dentist("Dennis Nine", "Wells", 3, "dennisnine", "dennisnine");
-
-            den1.DentistLogIn();
 
 
 
 
 
 
-
-            //Nurses Log In
-    
-            Nurse Nurse1 = new Nurse("Jane Doe", "Taunton", 1, "jane", "jane");
-            Nurse Nurse2 = new Nurse("Jane Two", "Taunton", 2, "janetwo", "janetwo");
-            Nurse Nurse3 = new Nurse("Jane Three", "Taunton", 3, "janethree", "janethree");
-            Nurse Nurse4 = new Nurse("Jane Four", "Street", 1, "janefour", "janefour");
-            Nurse Nurse5 = new Nurse("Jane Five", "Street", 2, "janefive", "janefive");
-            Nurse Nurse6 = new Nurse("Jane Six", "Street", 3, "janesix", "janesix");
-            Nurse Nurse7 = new Nurse("Jane Seven", "Wells", 1, "janeseven", "janeseven");
-            Nurse Nurse8 = new Nurse("Jane Eight", "Wells", 2, "janeeight", "janeeight");
-            Nurse Nurse9 = new Nurse("Jane Nine", "Wells", 3, "janenine", "janenine");
-
-
-            Nurse1.NurseLogIn();
 
 
         }
     }
 }
+
+
--- practice.cs
+++ practice.cs
@@ -8,5 +8,32 @@ namespace CW2
 {
     class practice
     {
+        public string location;
+        public string address;
+        public string key;
+        public string val;
+
+        //Dictionary to store location and address.
+
+        Dictionary<string, string> practiceInfo = new Dictionary<string, string>()
+        {
+            {"Taunton","4 High Street, TA2 0ND"},
+            {"Street","2 High Street, BA16 0QN"}
+        };
+
+
+        //Method to allow user input to add practice.
+
+        public void addPractice()
+        {
+            Console.WriteLine("Enter the LOCATION of the new practice");
+            key = Console.ReadLine();
+            Console.WriteLine("Enter ADDRESS of the new practice");
+            val = Console.ReadLine();
+            practiceInfo.Add(key,val); //adds input to dictionary.
+
+
+        }
+
     }
 }
